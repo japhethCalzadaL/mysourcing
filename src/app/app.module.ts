121180b5f74da591cde7182b3038a9010e1f39db
japhethCalzadaL <japhethoz@gmail.com>
2024-03-06 20:28:10 -0600

    ngForm error solution

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { UserFormComponent } from './users/user-form/user-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    UserFormComponent
   ],
   imports: [
     BrowserModule,
